ec26458789cb4ddc5e40fc0d0a252706fcaef1c2
Hardy Miranda <hardy.miranda@picpay.com>
2022-08-26 17:51:46 -0300

    Add neotest and which-key plugins

---
+++ vim/nvim-lua/lua/configs/test.lua
@@ -0,0 +1,14 @@
+require('neotest').setup({
+  adapters = {
+    require('neotest-vim-test')({
+      ignore_file_types = {
+        'vim',
+        'lua'
+      },
+      -- allow_file_types = {
+      --   'haskell',
+      --   'elixir'
+      -- }
+    })
+  }
+})
